65a4e0921939c12411d46056d6dfaffa10391f7c
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-10 22:52:08 +0300

    Solution to problem P18LeapYearChecker

---
+++ P02ConditionalStatements/src/P18LeapYearChecker.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class P18LeapYearChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = Integer.parseInt(scanner.nextLine());
+        String result = "";
+        if (year % 100 == 0){
+            if (year % 400 == 0){
+                result = "It`s a leap year!";
+            } else {
+                result = "It`s not a leap year!";
+            }
+        } else if (year % 4 == 0){
+            result = "It`s a leap year!";
+        } else {
+            result = "It`s not a leap year!";
+        }
+
+        System.out.printf("%s", result);
+    }
+}
