7271692dfaee1f1cacb6667e20a83ce78104e655
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-10 23:02:19 +0300

    Solution to problem P19MovieTicketPrice

---
+++ P02ConditionalStatements/src/P19MovieTicketPrice.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class P19MovieTicketPrice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = Integer.parseInt(scanner.nextLine());
+        String errorMessage = "Age greater then -1";
+        double ticketPrice = 0.0;
+        if (!(age >= 0)){
+            System.out.printf("%s", errorMessage);
+        } else if (age <= 12){
+            ticketPrice = 5;
+        } else if (age <= 19){
+            ticketPrice = 8;
+        } else {
+            ticketPrice = 10;
+        }
+
+        System.out.printf("The ticket price is: $%.2f", ticketPrice);
+    }
+}
