34128699d48f687d4351ec498661de8e1ae8146c
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-10 22:17:53 +0300

    Solution to problem P17GradeCalculator

---
+++ P02ConditionalStatements/src/P17GradeCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class P17GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int percentage = Integer.parseInt(scanner.nextLine());
+        String grade = "";
+        String errorMessage = "Invalid percentage";
+        if (!(percentage <= 100 && percentage >= 0)){
+            System.out.printf("%s", errorMessage);
+            return;
+        } else if (percentage >= 90){
+            grade = "A";
+        } else if (percentage >= 80){
+            grade = "B";
+        } else if (percentage >= 70){
+            grade = "C";
+        } else if (percentage >= 60){
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.printf("The grade is: %s", grade);
+    }
+}
