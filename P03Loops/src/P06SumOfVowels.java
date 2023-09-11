45e7ecab6a2b3fc36c7d52d8a68d279d7bb81953
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-11 15:19:48 +0300

    Solution to problem P06SumOfVowels

---
+++ P03Loops/src/P06SumOfVowels.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P06SumOfVowels {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String letter = scanner.nextLine();
+        int totalSum = 0;
+        for (int i = 0; i < letter.length(); i++){
+            char symbol = letter.charAt(i);
+            if (symbol == 'a'){
+                totalSum += 1;
+            } else if (symbol == 'e'){
+                totalSum += 2;
+            } else if (symbol == 'i'){
+                totalSum += 3;
+            } else if (symbol == 'o'){
+                totalSum += 4;
+            } else if (symbol == 'u'){
+                totalSum += 5;
+            }
+        }
+
+        System.out.printf("%d", totalSum);
+    }
+}
