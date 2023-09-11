0bcfd6c77492daf44b4b65eb84396fdcd839b168
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-11 21:22:39 +0300

    Solution to problem P10FactorialCalculation

---
+++ P03Loops/src/P11NumberReversal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P11NumberReversal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+        int revers = 0;
+        while (inputNumber != 0){
+            int reminder = inputNumber % 10;
+            revers = revers * 10 + reminder;
+            inputNumber = inputNumber / 10;
+        }
+
+        System.out.printf("%d", revers);
+    }
+}
