de31668ba510aa01da0a80c392ceda56f4dbb556
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-11 18:50:37 +0300

    Solution to problem P10FactorialCalculation

---
+++ P03Loops/src/P10FactorialCalculation.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P10FactorialCalculation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+        int sumFactorial = 1;
+        for (int i = 1; i <= inputNumber; i++){
+            sumFactorial *= i;
+        }
+
+        System.out.println(sumFactorial);
+    }
+}
