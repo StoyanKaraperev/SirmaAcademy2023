b856bb6f79c1016563f0fa3c32b263a76dd8508d
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-11 14:58:02 +0300

    Solution to problem P04Numbers1ToNThroughM

---
+++ P03Loops/src/P04Numbers1ToNThroughM.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class P04Numbers1ToNThroughM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int loops = Integer.parseInt(scanner.nextLine());
+        int step = Integer.parseInt(scanner.nextLine());
+        String errorMessage = "Invalid input";
+        if (loops < 0 || step < 0){
+            System.out.printf("%s", errorMessage);
+        } else {
+            for (int i = 1; i <= loops; i+=step){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
