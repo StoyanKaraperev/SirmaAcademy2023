acc289b7c71a367ee8cd2268ccfff77fdf8c481f
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-11 14:45:20 +0300

    Solution to problem P02NumbersNTo0InReverseOrder

---
+++ P03Loops/src/P02NumbersNTo0InReverseOrder.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class P02NumbersNTo0InReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputLoops = Integer.parseInt(scanner.nextLine());
+        if (inputLoops >= 0){
+            for (int i = inputLoops; i >= 0; i--){
+                System.out.println(i);
+            }
+        } else {
+            System.out.println("Invalid input!");
+        }
+    }
+}
