6924bf60b7240d2de4bb9c08a8fa87978d56c264
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-12 00:08:19 +0300

    Solution to problem P14ArmstrongNumberCheck

---
+++ P03Loops/src/P14ArmstrongNumberCheck.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P14ArmstrongNumberCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputNumber = scanner.nextLine();
+        int expectedNumber = Integer.parseInt(inputNumber);
+        int originalNumber = Integer.parseInt(inputNumber);
+        int power = inputNumber.length();
+        int reminder = 0;
+        int result = 0;
+        while (originalNumber != 0){
+            reminder = originalNumber % 10;
+            result += Math.pow(reminder, power);
+            originalNumber /= 10;
+        }
+
+        if (result == expectedNumber){
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+    }
+}
