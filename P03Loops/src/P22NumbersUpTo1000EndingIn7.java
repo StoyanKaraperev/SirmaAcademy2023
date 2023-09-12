924f546aac08ce970d12e325749cc8e148e63d77
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-12 22:17:29 +0300

    Solution to problem P22NumbersUpTo1000EndingIn7

---
+++ P03Loops/src/P22NumbersUpTo1000EndingIn7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P22NumbersUpTo1000EndingIn7 {
+    public static void main(String[] args) {
+        int remainder = 0;
+        for (int i = 1; i <= 1000; i++){
+            remainder = i % 10;
+            if (remainder == 7){
+                System.out.printf("%d %n", i);
+            }
+        }
+
+    }
+}
