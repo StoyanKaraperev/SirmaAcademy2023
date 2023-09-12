436dc93a6eb7bcf2415599d78184b9601aa45a8e
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-12 22:45:24 +0300

    Solution to problem P24Encoding

---
+++ P03Loops/src/P24Encoding.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P24Encoding {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int AddNumber = 33;
+        int inputNumber = Integer.parseInt(scanner.nextLine());
+        int reminder;
+        int numToSymbol;
+        char symbol;
+        while (inputNumber != 0){
+            reminder = inputNumber % 10;
+            if (reminder == 0) {
+                System.out.print("ZERO");
+            }
+            numToSymbol = reminder + AddNumber;
+            symbol = (char) numToSymbol;
+            for (int i = 0; i < reminder; i++){
+                System.out.printf("%s", symbol);
+            }
+            System.out.println();
+            inputNumber /= 10;
+        }
+    }
+}
