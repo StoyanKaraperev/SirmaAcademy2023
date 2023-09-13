8ae48a137740f64c0c6060ec8e52d8c0f2b528a4
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-13 12:31:59 +0300

    Solution to problem P28PyramidOfNumbers

---
+++ P03Loops/src/P28PyramidOfNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P28PyramidOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputNumbers = Integer.parseInt(scanner.nextLine());
+        int count = 1;
+        for (int row = 1; row <= inputNumbers; row++){
+            for (int col = 1; col <= row; col++){
+                System.out.print(count + " ");
+                count++;
+                if (count > inputNumbers){
+                    return;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
