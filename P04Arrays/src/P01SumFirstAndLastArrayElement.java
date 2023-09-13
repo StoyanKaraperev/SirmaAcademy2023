ed257a4d80d3a8ab434b43cf7b38b3efbad1158a
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-13 20:41:24 +0300

    Solution to problem P01SumFirstAndLastArrayElement

---
+++ P04Arrays/src/P01SumFirstAndLastArrayElement.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01SumFirstAndLastArrayElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] arrNumbers = Arrays
+                .stream(scanner.nextLine().split(","))
+                .mapToInt((e -> Integer.parseInt(e)))
+                .toArray();
+        int totalSum = 0;
+        totalSum = arrNumbers[0] + arrNumbers[arrNumbers.length - 1];
+        System.out.println(totalSum);
+    }
+}
