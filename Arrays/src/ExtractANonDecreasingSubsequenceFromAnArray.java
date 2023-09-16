eab9c1893e2b0bf62688742986de3a336995566a
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-16 17:44:43 +0300

    Solution of tha task ExtractANonDecreasingSubsequenceFromAnArray

---
+++ Arrays/src/ExtractANonDecreasingSubsequenceFromAnArray.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class ExtractANonDecreasingSubsequenceFromAnArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int [] inputArray = Arrays
+                .stream(scanner.nextLine()
+                        .trim()
+                        .split(", "))
+                .mapToInt(e -> Integer.parseInt(e))
+                .toArray();
+        int theLargestNumber = inputArray[0];
+        int count = 0;
+        List<Integer> increasingSubsequence = new ArrayList<Integer>();
+        for (int i = 0; i < inputArray.length; i++) {
+            if (count == 0){
+                increasingSubsequence.add(theLargestNumber);
+            } else {
+                count = 0;
+            }
+            for (int j = i + 1; j < inputArray.length ; j++) {
+                if ( theLargestNumber < inputArray[j]){
+                    theLargestNumber = inputArray[j];
+                    break;
+                } else {
+                    count++;
+                    break;
+                }
+            }
+
+
+        }
+        for (int num : increasingSubsequence) {
+            System.out.printf("%d ", num);
+        }
+    }
+}
