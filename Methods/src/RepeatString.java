c21b88889bc4b1bd2591f7983d2e7d1ce5ec9a8a
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-20 01:36:47 +0300

    Solution for task RepeatString

---
+++ Methods/src/RepeatString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RepeatString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        int repeatNumber = Integer.parseInt(scanner.nextLine());
+        returnNewStringRepeatedNTimesInOneLine(inputString, repeatNumber);
+    }
+
+    public static void returnNewStringRepeatedNTimesInOneLine(String stringToRepeat, int numberToRepeat){
+        for (int i = 0; i < numberToRepeat; i++) {
+            System.out.printf("%s", stringToRepeat);
+        }
+    }
+}
