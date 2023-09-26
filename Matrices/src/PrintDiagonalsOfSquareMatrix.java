fda7f86aba2682189b8292821570ac2ccb4f3302
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-09-26 17:58:01 +0300

    Solution for task PrintDiagonalsOfSquareMatrix

---
+++ Matrices/src/PrintDiagonalsOfSquareMatrix.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintDiagonalsOfSquareMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rowCol = Integer.parseInt(scanner.nextLine());
+        String [][] matrix = new String[rowCol][rowCol];
+
+        fillTheMatrix(rowCol, matrix, scanner);
+
+        ArrayList<String> firstDiagonal = new ArrayList<>();
+        ArrayList<String> secondDiagonal = new ArrayList<>();
+        int count = 0;
+        for (int row = 0; row < matrix.length; row++) {
+            firstDiagonal.add(matrix[row + count][row + count]);
+        }
+
+        for (int row = matrix.length - 1; row >= 0; row--) {
+            secondDiagonal.add(matrix[row][count]);
+            count++;
+        }
+
+        printTheResult(firstDiagonal);
+        printTheResult(secondDiagonal);
+    }
+
+    private static void printTheResult (ArrayList<String> words){
+        for (String word : words) {
+            System.out.printf("%s ", word);
+        }
+        System.out.println();
+    }
+    private static void fillTheMatrix(int rowCol, String[][] matrix, Scanner scanner) {
+        for (int row = 0; row < rowCol; row++) {
+            matrix[row] = scanner.nextLine()
+                    .trim()
+                    .split("\\s+");
+        }
+    }
+}
