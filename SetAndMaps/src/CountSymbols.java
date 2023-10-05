d0b0cedda50f702f4542b5a0b2d6e39cb0d174c6
Stoyan Karaperev <stoqn.karaperev@gmail.com>
2023-10-05 16:32:35 +0300

    Solution for exercise CountSymbols

---
+++ SetAndMaps/src/CountSymbols.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class CountSymbols {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String [] inputText = scanner.nextLine()
+                .trim()
+                .split("");
+        Map<String, Integer> result = new TreeMap<>();
+        for (String symbol : inputText) {
+            if (!result.containsKey(symbol)){
+                result.put(symbol, 1);
+            } else {
+                result.put(symbol, result.get(symbol) + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> print : result.entrySet()) {
+            String symbol = print.getKey();
+            int count = print.getValue();
+            System.out.printf("%s : %d%n", symbol, count);
+        }
+    }
+}
